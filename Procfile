1e3ea4d5c6405dd7521b3f1f9aeea3c149e95905
Ligalaiz <slavakuz88@gmail.com>
2022-06-24 23:57:15 +0300

    fix: change heroku config

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node index.js
+web: node index.js --experimental-json-modules --exec babel-node --presets babel-preset-env
--- Router/Router.js
+++ Router/Router.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
-import * as swaggerDocument from '../swagger/openapi.json';
+import * as swaggerDocument from '../swagger/openapi.json' assert { type: 'json' };
 import { controller } from '../Controller/index.js';
 
 const router = express.Router();
 
 router.post('/feedback', controller.send);
 router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+router.get('/api-docs', swaggerUi.setup(swaggerDocument.default));
 
 export { router };
